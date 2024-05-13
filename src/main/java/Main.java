7a6ecca1456fe752ba12e655da85caf8a55f2f96
leonardo roberto <33862566-borgespereirale@users.noreply.replit.com>
2024-05-13 15:08:27 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("cantor:eminem");
+
+    System.out.println("album:the eminem show ano de lançamento em 2002");
+    System.out.println("The Eminem Show é o terceiro álbum de estúdio do rapper Eminem, lançado em 2002.Without Me 4:50,white america 5:24,business 4:11");
+  }
+  
+}
